de1386a54c1adc62b43fd4daa49fa279c3c675fa
tb96 <bueschken@team-digital.de>
2017-11-16 14:52:01 +0100

    Add files via upload

--- installTypo3.sh
+++ installTypo3.sh
@@ -87,7 +87,19 @@ printf 'beta:$1$$.OPcLRctp0tpQ81Db9tKP/' >> .htpasswd
 cd cms/
 
 #create index.php
-ln -s typo3_src/index.php index.php
+#ln -s typo3_src/index.php index.php
+touch index.php
+printf "
+<?php
+// Replacement for symlinked index.php file for TYPO3
+// Author: ziegenhain@team-digital.de
+// Version 1.0 – 01-02-2015
+
+// Since we don't allow symlinks to files (security reasons, only to directories)
+// we include typo3_src/index.php in this script, therefore symlink to file is
+// not needed and still the current version of the TYPO3 index.php is loaded.
+
+require 'typo3_src/index.php';" >> index.php
 
 #create fileadmin, user_upload and typo3conf
 mkdir fileadmin
